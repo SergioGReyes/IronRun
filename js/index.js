49abfcfd7fb8ccc4c431cb29003afec220141b1d
RuddyLotar <ruddylotar@gmail.com>
2018-11-07 00:13:00 +0100

    Main Title modification

--- js/index.js
+++ js/index.js
@@ -5,11 +5,10 @@ window.onload = function () {
   ctx = canvas.getContext("2d");
   var img = new Image();
   var imgEnter = new Image();
-  img.src = './img/sports-stadium.jpg'
-  imgEnter.src = './img/enterkey.png'
+  img.src = './img/MainTitle.jpg'
+  
   img.onload = function () {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(imgEnter, 900, 450, 200, 180);
   };
 
   document.onkeyup = function (event) {
